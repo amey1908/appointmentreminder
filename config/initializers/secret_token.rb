1ecc5c815b0c39a79b7aad284b4a472887401b67
amey1908 <amey420@gmail.com>
2014-02-09 18:42:05 -0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Appointmentreminder::Application.config.secret_key_base = 'f007501b3d49e8ba85da5f86a5c8b791f06c039f444c1b8d943bcf0c83e994b07acffa0c94d415b71c34559c179ccd58a52eda76204db17b5785584f154f2137'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Appointmentreminder::Application.config.session_store :cookie_store, key: '_appointmentreminder_session'
